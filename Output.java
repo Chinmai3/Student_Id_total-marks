bb246e870e2c4b7b641046d9dd4d5432fbed9966
Chinmai3 <123291366+Chinmai3@users.noreply.github.com>
2023-04-01 12:32:13 +0530

    Create Output.java

---
+++ Output.java
@@ -0,0 +1,19 @@
+Enter number of students
+3
+Enter the no of subjects
+3
+Enter Student id:1
+Enter marks of the students 
+89 85 90
+Enter Student id:2
+Enter marks of the students 
+89 94 90
+Enter Student id:3
+Enter marks of the students 
+78 90 97
+Student data and their marks are:
+{1=[89, 85, 90], 2=[89, 94, 90], 3=[78, 90, 97]}
+Enter the student id whose total marks you want to calculate
+3
+Print student id and their total marks:
+Student ID: 3 Total Marks: 265
